0fdebe7936a4d41694fb13691703022944949d95
Your Name <you@example.com>
2023-07-08 01:06:08 +0530

    Added draw function and minor other changes

--- solution.py
+++ solution.py
@@ -1,14 +1,28 @@
 import pygame
+
 pygame.init()
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+def draw(win):
+    win.fill(BLACK)
+    pygame.display.update()
+
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
+        draw(WIN)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -16,3 +30,6 @@ def main():
 
     pygame.quit()
 
+
+if __name__ == '__main__':
+    main()

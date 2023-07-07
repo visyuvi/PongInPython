55c16eb4496cf3ff9bd83fb5b709fb2765d1c4ce
Your Name <you@example.com>
2023-07-08 00:16:26 +0530

    Initial commit

---
+++ solution.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+

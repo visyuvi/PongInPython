9fad27655a9500043acf562f396a47ee9582002f
Your Name <you@example.com>
2023-07-08 12:55:36 +0530

    Added Ball class. Added code to move the ball in x direction starting from center of the board

--- solution.py
+++ solution.py
@@ -11,6 +11,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
 
 
 class Paddle:
@@ -33,16 +34,34 @@ class Paddle:
             self.y += self.VEL
 
 
-def draw(win, paddles):
+class Ball:
+    MAX_VEL = 5
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, WHITE, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+
+def draw(win, paddles, ball):
     win.fill(BLACK)
     for paddle in paddles:
         paddle.draw(win)
 
-    for i in range(10, HEIGHT, HEIGHT//20):
+    for i in range(10, HEIGHT, HEIGHT // 20):
         if i % 2 == 1:
             continue
-        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
-
+        pygame.draw.rect(win, WHITE, (WIDTH // 2 - 5, i, 10, HEIGHT // 20))
+    ball.draw(win)
     pygame.display.update()
 
 
@@ -63,16 +82,17 @@ def main():
 
     left_paddle = Paddle(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
-
+    ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
+        ball.move()
     pygame.quit()
 
 
